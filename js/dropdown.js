46c7805c6debb3878530f94855a22c62fb794b12
Zak <zakrowland@yahoo.com>
2022-08-04 17:24:25 -0700

    Converted header to use CSS grid
    I kept going back and forth on this but CSS grid will make responsive design easier

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,11 +1,11 @@
 function toggleDropdown() {
     var button_img = document.getElementById("dropdown-icon");
-    var dropdown = document.getElementById("mobile-nav-dropdown");
-    if (dropdown.style.display === "flex") {
+    var dropdown = document.getElementById("nav-buttons");
+    if (dropdown.style.display === "inherit") {
         dropdown.style.display = "none";
         button_img.setAttribute("src", "/img/drop-down-icon.png")
     } else {
-        dropdown.style.display = "flex";
+        dropdown.style.display = "inherit";
         button_img.setAttribute("src", "/img/drop-down-icon-x.png")
     }
 }
